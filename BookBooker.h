87701d85c712311cddb800796ac1dec627c3336e
Vladyslav2020 <vmskl2020@gmail.com>
2021-03-31 22:27:41 +0300

    Добавил BookBooker, BookingUserInterface.

---
+++ BookBooker.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "Book.h"
+
+class BookBooker {
+public:
+	void bookTheBook(Book* book) {
+
+	}
+};
---
+++ BookingUserInterface.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "Book.h"
+
+class BookingUserInterface {
+public:
+	void startDialogMenu();
+	void showBooks();
+	void bookingTheBook();
+};
