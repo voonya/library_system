3bae90ada4b72d579a39096ebf8f4a9aa4ea872e
Sviatozar <svpetrenko123@gmail.com>
2021-04-14 18:16:51 +0300

    Change BookProcessingMenu not to ask the number of the book, but its title

--- library_system/BooksProcessingMenu.h
+++ library_system/BooksProcessingMenu.h
@@ -5,7 +5,9 @@ class BooksProcessingMenu {
 public:
 	void startDialogMenu(BookDatabase& DB);
 	void showBooks(BookDatabase& DB);
+	void showBooks(vector<Book> DB);
 	void addNewBook(BookDatabase& DB, string title, string author, int year, int pages, string additionalInfo);
 	void editBook(BookDatabase& DB, int bookNumber, string newTitle, string newAuthor, int newYear, int newPages, string newAdditionalInfo);
 	void removeBook(BookDatabase& DB, int bookNumber);
+	int request_book(int& number, BookDatabase& DB);
 };
