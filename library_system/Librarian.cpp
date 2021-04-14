103387717c4e6915d4a46bafc07832b16f4b7575
Sviatozar <svpetrenko123@gmail.com>
2021-04-14 20:19:32 +0300

    add Librarian class

---
+++ library_system/Librarian.cpp
@@ -0,0 +1,20 @@
+#include "Librarian.h"
+#include <string>
+using namespace std;
+
+Librarian::Librarian(string name, string surname, string phone)
+{
+	this->name = name;
+	this->surname = surname;
+	this->phone_number = phone;
+}
+
+string get_more_info(Librarian c) {
+	string info = " Phonenumber: " + c.phone_number;
+	return info;
+}
+
+string get_main_info(Client c) {
+	string info = " Name: " + c.name + "\n Surname: " + c.surname;
+	return info;
+}
