6e1a9a5b023669629c1708292c1461425edf6e2a
Vladyslav2020 <vmskl2020@gmail.com>
2021-04-15 09:21:43 +0300

    Добавил меню для бронирования пользователей

--- library_system/BookingUserMenu.h
+++ library_system/BookingUserMenu.h
@@ -7,11 +7,7 @@
 class BookingUserMenu {
 public:
 	void startDialogMenu(BookDatabase& DB, BookBooker& bookBooker, ClientDatabase& cDB);
-<<<<<<< HEAD
-	void showBookingsMenuUser(BookDatabase& DB, BookBooker& bookBooker, ClientDatabase& cDB, Client* client);
-=======
 	void showBookingsMenuUser(BookDatabase& DB, BookBooker& bookBooker, Client* client);
->>>>>>> 2f35ff9cdb69ca68e7413f9e22b09456b17411b4
 	void showBooks(BookDatabase& DB);
 	void showBookings(BookDatabase& DB);
 	void bookingTheBook(BookDatabase& DB, BookBooker& bookBooker, int bookNumber, Client* client);
